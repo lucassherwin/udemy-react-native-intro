36c992d1834e37cbce333e6108f6b2d4afdf3db6
lucassherwin <lksherwin@gmail.com>
2020-08-10 17:02:42 -0400

    layout exercise

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,14 +4,15 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.text1Style}>Child #1</Text>
+            {/* <Text style={styles.text1Style}>Child #1</Text>
             <Text style={styles.text2Style}>Child #2</Text>
-            <Text style={styles.text3Style}>Child #3</Text>
+            <Text style={styles.text3Style}>Child #3</Text> */}
 
 
             <View style={styles.view1Style}/>
-            <View style={styles.view3Style}/>
             <View style={styles.view2Style}/>
+            <View style={styles.view3Style}/>
+            
         </View>
     )
 }
@@ -20,7 +21,9 @@ const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 3,
         borderColor: 'black',
-        height: 200
+        height: 200,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
     text1Style: {
         borderWidth: 3,
@@ -37,15 +40,20 @@ const styles = StyleSheet.create({
         borderColor: 'red',
     },
     view1Style: {
-        height: 5,
-        width: 5,
-        backgroundColor: 'red'
+        height: 50,
+        width: 50,
+        backgroundColor: 'red',
     },
     view2Style: {
-        
+        height: 50,
+        width: 50,
+        backgroundColor: 'green',
+        top: 50
     },
     view3Style: {
-        
+        height: 50,
+        width: 50,
+        backgroundColor: 'purple'
     }
 })
 
